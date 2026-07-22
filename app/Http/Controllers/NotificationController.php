4f74216afac363624274a816d796a114edfb2844
LamaAlHalaqi <lamaalhalaqi37@gmail.com>
2026-07-22 20:49:42 +0300

    show Notifications unread with details

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -11,23 +11,47 @@ class NotificationController extends Controller
     /**
      * Get logged user notifications
      */
-  public function index(Request $request)
+ public function index(Request $request)
 {
     $user = Auth::user();
 
     if ($user->hasRole('admin')) {
 
         $notifications = DB::table('notifications')
-            ->orderByRaw("CASE WHEN status = 'sent' THEN 0 ELSE 1 END")
-            ->orderBy('created_at', 'desc')
+            ->leftJoin('finished_product_batches',
+                'notifications.batch_id', '=', 'finished_product_batches.id'
+            )
+            ->leftJoin('finished_products',
+                'finished_product_batches.finished_product_id', '=', 'finished_products.id'
+            )
+            ->select(
+                'notifications.*',
+                'finished_product_batches.expiry_date',
+                'finished_product_batches.remaining_quantity',
+                'finished_products.name as product_name'
+            )
+            ->orderByRaw("CASE WHEN notifications.status = 'sent' THEN 0 ELSE 1 END")
+            ->orderBy('notifications.created_at', 'desc')
             ->get();
 
     } elseif ($user->hasRole('product_storekeeper')) {
 
         $notifications = DB::table('notifications')
-            ->where('user_id', $user->id)
-            ->orderByRaw("CASE WHEN status = 'sent' THEN 0 ELSE 1 END")
-            ->orderBy('created_at', 'desc')
+            ->where('notifications.user_id', $user->id)
+            ->leftJoin('finished_product_batches',
+                'notifications.batch_id', '=', 'finished_product_batches.id'
+            )
+            ->leftJoin('finished_products',
+                'finished_product_batches.finished_product_id', '=', 'finished_products.id'
+            )
+            ->select(
+                'notifications.*',
+                'finished_product_batches.expiry_date',
+                'finished_product_batches.remaining_quantity',
+                'finished_products.name as product_name'
+            )
+            ->orderByRaw("CASE WHEN notifications.status = 'sent' THEN 0 ELSE 1 END")
+            ->orderBy('notifications.created_at', 'desc')
             ->get();
 
     } else {
@@ -44,7 +68,6 @@ class NotificationController extends Controller
         'data' => $notifications
     ]);
 }
-
     /**
      * Mark notification as read
      */
